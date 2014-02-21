1f831de3618df43c23903be60ff4591fa9b6bd2a
Paul Lam <paul@quantisan.com>
2014-02-21 11:27:57 -0500

    spreadsheet.next_append_row

--- controller/spreadsheet.py
+++ controller/spreadsheet.py
@@ -1,10 +1,36 @@
 import gspread
 import os
+import string
+
+
+def new_token():
+    gc = gspread.login(os.environ.get('GOOGLE_USER'),
+                       os.environ.get('GOOGLE_PASS'))
+    return gc
 
 
 def write_cell(worksheetKey, row, col, value):
-    gc = gspread.login(os.environ.get('GOOGLE_USER'),
-                       os.environ.get('GOOGLE_PASS'))
+    gc = new_token()
     worksheet = gc.open_by_key(worksheetKey).get_worksheet(0)
     if(worksheet.acell(col + row).value is None):
         worksheet.update_acell(col + row, value)
+
+
+def col2num(col):
+    """
+    Converts a spreadsheet column letter name to a column number
+
+    Reference:
+    http://stackoverflow.com/a/12640614/654416
+    """
+    num = 0
+    for c in col:
+        if c in string.ascii_letters:
+            num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+    return num
+
+
+def next_append_row(worksheetKey, col):
+    gc = new_token()
+    worksheet = gc.open_by_key(worksheetKey).get_worksheet(0)
+    return len(worksheet.col_values(col2num(col))) + 1
--- test/spreadsheet_test.py
+++ test/spreadsheet_test.py
@@ -1,11 +1,22 @@
 from controller import spreadsheet as ss
 import gspread
 import unittest
+import os
+
+WORKSHEET = os.environ.get('TEST_WORKSHEET_KEY')
 
 
 class SpreadsheetTest(unittest.TestCase):
+    def setUp(self):
+        """Wipe first column in the test worksheet."""
+        gc = ss.new_token()
+        worksheet = gc.open_by_key(WORKSHEET).get_worksheet(0)
+        cell_list = worksheet.range('A1:A100')
+        for cell in cell_list:
+            cell.value = ''
+
     def test_write_cell(self):
-        self.assertIsNone(ss.write_cell("0AlVXob3noRJTdFV5b3piVU03LU1zaEVObU4wMXViSmc",
+        self.assertIsNone(ss.write_cell(WORKSHEET,
                                         "1",
                                         "A",
                                         "this is test"))
@@ -17,3 +28,12 @@ class SpreadsheetTest(unittest.TestCase):
                           "1",
                           "A",
                           "this is test")
+
+    def test_col2num(self):
+        self.assertEqual(ss.col2num("A"), 1)
+        self.assertEqual(ss.col2num("Z"), 26)
+        self.assertEqual(ss.col2num("AA"), 27)
+
+    def test_next_append_row(self):
+        self.assertEqual(ss.next_append_row(WORKSHEET, "A"),
+                         3)
