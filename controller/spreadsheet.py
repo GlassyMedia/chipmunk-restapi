53ba47181b76b0c3bc5f2c55450abfc6d8ea21af
Paul Lam <paul@quantisan.com>
2014-02-21 10:55:53 -0500

    use worksheet key instead of worksheet name

--- controller/spreadsheet.py
+++ controller/spreadsheet.py
@@ -2,9 +2,9 @@ import gspread
 import os
 
 
-def write_cell(worksheet, row, col, value):
-    account = gspread.login(os.environ.get('GOOGLE_USER'),
-                            os.environ.get('GOOGLE_PASS'))
-    worksheet = account.open(worksheet).sheet1
+def write_cell(worksheetKey, row, col, value):
+    gc = gspread.login(os.environ.get('GOOGLE_USER'),
+                       os.environ.get('GOOGLE_PASS'))
+    worksheet = gc.open_by_key(worksheetKey).get_worksheet(0)
     if(worksheet.acell(col + row).value is None):
         worksheet.update_acell(col + row, value)
--- test/spreadsheet_test.py
+++ test/spreadsheet_test.py
@@ -1,10 +1,19 @@
 from controller import spreadsheet as ss
+import gspread
 import unittest
 
 
 class SpreadsheetTest(unittest.TestCase):
     def test_write_cell(self):
-        self.assertIsNone(ss.write_cell("chipmunk testing",
+        self.assertIsNone(ss.write_cell("0AlVXob3noRJTdFV5b3piVU03LU1zaEVObU4wMXViSmc",
                                         "1",
                                         "A",
                                         "this is test"))
+
+    def test_write_worksheet_not_found(self):
+        self.assertRaises(gspread.SpreadsheetNotFound,
+                          ss.write_cell,
+                          "xxx1234",
+                          "1",
+                          "A",
+                          "this is test")
