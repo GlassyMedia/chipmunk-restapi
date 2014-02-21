29e19cc31a8909af166501195cfcd9c8e0702474
Paul Lam <paul@quantisan.com>
2014-02-21 10:34:51 -0500

    server POST to cell working

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
-from bottle import Bottle, run
-import spreadsheet
+from bottle import Bottle, run, request, response
+from controller import spreadsheet as ss
 
 app = Bottle()
 
 
-@app.route('/api/v1/cell', method='POST')
+@app.post('/api/v1/cell')
 def post_cell():
-    data = app.request.json
-    spreadsheet.write_cell(data["worksheet"],
-                           data["row"],
-                           data["col"],
-                           data["value"])
+    """
+    Writes to a spreadsheet cell.
+
+    POST a JSON with 'worksheet', 'row', 'col', and 'value' keys.
+    HTTP request header must set Content-Type to application/json.
+    """
+    data = request.json
+    attr = ('worksheet', 'row', 'col', 'value')
+    if data is not None and all(key in data for key in attr):
+        ss.write_cell(data["worksheet"],
+                      data["row"],
+                      data["col"],
+                      data["value"])
+        response.status = '201 Created'
+    else:
+        response.status = '400 Bad Request'
+
+    return response
 
 if __name__ == "__main__":
-    run(app, host='localhost', port=8080)
+    run(app, host='localhost', port=8080, reloader=True, debug=True)
--- test/server_test.py
+++ test/server_test.py
@@ -1,12 +1,26 @@
-#from webtest import TestApp
-#import server
+from webtest import TestApp
+import unittest
+import server
+
+app = TestApp(server.app)
 
 
-#def test_post_cell():
-    #app = TestApp(server.app)
+class TestWriteCell(unittest.TestCase):
+    def test_post_cell(self):
+        response = app.post_json('/api/v1/cell',
+                                 {'worksheet': 'chipmunk testing',
+                                  'row': '2',
+                                  'col': 'A',
+                                  'value': 'unit test pass'})
+        self.assertEqual(response.status, "201 Created")
 
-    #response = app.post('/api/v1/cell?worksheet=testsheet&cellRow=1&cellCol=A',
-                        #{'username': 'foo',
-                         #'password': 'bar',
-                         #'value': 'abc'})
-    #assert response.status == "200 OK"
+    def test_post_cell_missing_keys(self):
+        response = app.post_json('/api/v1/cell',
+                                 {'worksheet': 'chipmunk testing',
+                                  'col': 'A'},
+                                 status=400)
+        self.assertEqual(response.status, "400 Bad Request")
+
+    def test_post_cell_nojson(self):
+        response = app.post('/api/v1/cell', status=400)
+        self.assertEqual(response.status, "400 Bad Request")
